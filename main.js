9456b6c35b8e6e7d5739ac6c5df260c73c04189d
hpozzy <oscargallegostapia@gmail.com>
2020-09-25 16:03:38 -0700

    friday

---
+++ main.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+  document.getElementById("buttonModal").onclick = function () {
+    document.getElementById("modal").style.display = "none"
+  }
+}
+
+const cards = document.querySelectorAll(".cards")
+
+let hasFlipped = false
+let stopClicks = false
+let cardOne
+let cardTwo
+
+function flipCard() {
+  if (stopClicks) {
+    return
+  }
+  this.classList.add("showText")
+
+  if (!hasFlipped) {
+    hasFlipped = true
+    cardOne = this
+    return
+  } else {
+    cardTwo = this
+    hasFlipped = false
+
+    isMatch()
+  }
+}
+
+function isMatch() {
+  if (cardOne.dataset.title === cardTwo.dataset.title) {
+    keepShowing()
+    return
+  }
+  unflipCards()
+}
+
+function keepShowing() {
+  cardOne.removeEventListener("click", flipCard)
+  cardTwo.removeEventListener("click", flipCard)
+}
+
+function unflipCards() {
+  stopClicks = true
+
+  setTimeout(() => {
+    cardOne.classList.remove("showText")
+    cardTwo.classList.remove("showText")
+
+    stopClicks = false
+  }, 1500)
+}
+;(function shuffle() {
+  cards.forEach((card) => {
+    let ramdomPos = Math.floor(Math.random() * 18)
+    card.style.order = ramdomPos
+  })
+})()
+
+let lives = 8
+document.getElementById("livesLeft").innerHTML = "Lives Remaining: " + lives
+
+cards.forEach((card) => card.addEventListener("click", flipCard))
